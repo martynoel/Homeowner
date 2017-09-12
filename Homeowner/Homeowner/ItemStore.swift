339bb853f997b5395c40769a0f60a70ad9e9b267
Mimi Chenyao <mimichenyao@gmail.com>
2017-09-12 17:54:15 -0400

    Initial commit

---
+++ Homeowner/Homeowner/ItemStore.swift
@@ -0,0 +1,27 @@
+//
+//  ItemStore.swift
+//  Homeowner
+//
+//  Created by Mimi Chenyao on 9/12/17.
+//  Copyright © 2017 Mimi Chenyao. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    var allItems = [Item]()
+    
+    init() {
+        for _ in 0..<5 {
+            createItem()
+        }
+    }
+    
+    @discardableResult func createItem() -> Item {
+        let newItem = Item(random: true)
+        
+        allItems.append(newItem)
+        
+        return newItem
+    }
+}
---
+++ Homeowner/Homeowner/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Homeowner
+//
+//  Created by Mimi Chenyao on 9/12/17.
+//  Copyright © 2017 Mimi Chenyao. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    var itemStore: ItemStore!
+}
