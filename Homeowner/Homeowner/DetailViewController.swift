dea9e67562eb3bf24df98d8f72d3811c257b166f
Mimi Chenyao <mimichenyao@gmail.com>
2017-09-13 15:11:36 -0400

    "Added basic functionality"

---
+++ Homeowner/Homeowner/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  Homeowner
+//
+//  Created by Mimi Chenyao on 9/13/17.
+//  Copyright © 2017 Mimi Chenyao. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var serialNumberField: UITextField!
+    @IBOutlet var valueField: UITextField!
+    @IBOutlet var dateLabel: UILabel!
+}
